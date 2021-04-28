24d68ed96491a3600feb2a1eb2aa158938a97ad8
cui88 <1210432884@qq.com>
2021-04-28 11:35:15 +0800

    social_analysis.py modify commit

--- user_record.py
+++ user_record.py
@@ -3,7 +3,7 @@ import os
 import pandas as pd
 
 
-path = 'D:/guolab/湖大信息化工作/数据库用户/'
+path = 'F:/21年信息化办/数据库用户/'
 
 
 def GetFileDirList():
@@ -12,7 +12,7 @@ def GetFileDirList():
 
 
 def GetSystemPd():
-    dataPath = 'D:/guolab/湖大信息化工作/信息化办虚拟机总表(1).xlsx'
+    dataPath = 'F:/21年信息化办/信息化办虚拟机总表(1).xlsx'
     writer = pd.ExcelFile(dataPath)
     sheet_len = len(writer.sheet_names)
     system_pd = pd.DataFrame()
@@ -42,6 +42,7 @@ def getDict(df, key_name, value_name):
 def user_search(result_df):
     ip_list = []
     file_list = GetFileDirList()
+    # file_list = ['BTMON.csv']
     system_dict = getSystemDict()
     m_judge = 'from'
     for file in file_list:
@@ -50,52 +51,66 @@ def user_search(result_df):
         user_name = file[:-4]
         df = pd.read_csv(file_path, na_values='NAN', encoding='gb18030')
         for idx, row in df.iterrows():
+            # print(row['报文'])
             res = row['报文'].lower()
             # print('-----------------')
             # print(res)
             if not res.endswith(' '):
                 res += ' '
-            result = res.find(m_judge, 0, len(res)-1)
-            if result != -1:
-                # print(res)
-                res = res[result+5:len(res)-1]
-                # print('---'+ res)
-                for i in range(len(res)):
-                    if res[i] != ' ':
-                        break
-                result2 = res.find(' ', i, len(res)-1)
-                if result2 != -1:
-                    str = res[i:result2]
-                else:
-                    str = res[i:len(res)-1]
-                # print('----')
-                # print("str:"+str)
-                str = str.replace(" ", "")
-                view_judge = 'view'
-                if len(str) != 0:
-                    result3 = str.find('.', 0, len(str)-1)
-                    str2 = str
-                    if result3 != -1:
-                        str2 = str[result3+1:len(str)-1]
-                    # print("str2:" + str2)
-                    str3 = str2
-                    result4 = str2.find('"', 0, len(str2)-1)
-                    if result4 != -1:
-                        str3 = str2[result4+1:len(str2)-1]
-                    # print("str3:"+ str3)
-                    if str3[0] == 'v' or str3[1] == 'v' or view_judge in str3:
-                        ip = row['客户端IP']
-                        if ip in system_dict:
-                            system_name = system_dict[ip]
+            result2 = 0
+            while True:
+                # print("result2:%d"%result2)
+                if result2 != -1 and result2 != len(res)-1:
+                    # print(res)
+                    result = res.find(m_judge, result2, len(res)-1)
+                    # print(result)
+                    if result != -1:
+                        # print(res)
+                        res = res[result+5:]
+                        # print('---'+ res)
+                        for i in range(len(res)):
+                            if res[i] != ' ':
+                                break
+                        result2 = res.find(' ', i, len(res)-1)
+                        if result2 != -1:
+                            str = res[i:result2]
                         else:
-                            # print(ip)
-                            if ip not in ip_list:
-                                ip_list.append(ip)
-                            system_name = ''
-                        user_pd.loc[len(user_pd)] = [user_name, str3, ip, system_name]
+                            str = res[i:len(res)-1]
+                        # print('----')
+                        # print("str:"+str)
+                        res = res[i:]
+                        str = str.replace(" ", "")
+                        view_judge = 'view'
+                        if len(str) != 0:
+                            result3 = str.find('.', 0, len(str)-1)
+                            str2 = str
+                            if result3 != -1:
+                                str2 = str[result3+1:len(str)-1]
+                            # print("str2:" + str2)
+                            str3 = str2
+                            result4 = str2.find('"', 0, len(str2)-1)
+                            if result4 != -1:
+                                str3 = str2[result4+1:len(str2)-1]
+                            # print("str3:"+ str3)
+                            print(str3)
+                            if len(str3) != 0:
+                                if str3[0] == 'v' or view_judge in str3:
+                                    ip = row['客户端IP']
+                                    if ip in system_dict:
+                                        system_name = system_dict[ip]
+                                    else:
+                                        # print(ip)
+                                        if ip not in ip_list:
+                                            ip_list.append(ip)
+                                        system_name = ''
+                                    user_pd.loc[len(user_pd)] = [user_name, str3, ip, system_name]
+                    else:
+                        break
+                else:
+                    break
         user_pd = user_pd.drop_duplicates(subset='表名')
         result_df = pd.concat([user_pd, result_df])
-    result_df.to_excel('D:/guolab/湖大信息化工作/用户记录.xlsx', index=False, encoding='utf-8')
+    result_df.to_excel('F:/21年信息化办/用户记录.xlsx', index=False, encoding='utf-8')
     print(result_df.head())
     print(ip_list)
 
